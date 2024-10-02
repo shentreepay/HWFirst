2eb332d43b2db814051c5793fed43811708edb97
shentreepay <winhow17@gmail.com>
2024-10-02 20:46:48 +0800

    Java code

---
+++ P18.java
@@ -0,0 +1,12 @@
+package HW01;
+
+public class P18 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("顯示出反斜線:\\");
+		System.out.println("顯示出單引號\'");
+		System.out.println("八進位數的字元是\101");
+		System.out.println("十六進位數0061的字元是\u0061");
+	}
+}
---
+++ P26.java
@@ -0,0 +1,13 @@
+package HW01;
+
+public class P26 
+{
+	public static void main(String[] args) 
+	{
+		int num = 3;
+		System.out.println("變數num的值是"+num);
+		num = 5;
+		System.out.println("更新變數num的值");
+		System.out.println("變數num更新後的值"+num);
+	}
+}
